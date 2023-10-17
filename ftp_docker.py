600f5e9a46d8774bc2211bd1e4985101215ebdc4
Iqbal Rizky <ntaloventi@gmail.com>
2023-10-18 04:13:08 +0700

    edit param name

--- ftp_docker.py
+++ ftp_docker.py
@@ -1,7 +1,7 @@
 import os
 from ftplib import FTP, error_perm
 
-def ftp_upload(rem_up, rem_do, sc_up, dest_do, ftp_host, ftp_user, ftp_password, port=21):
+def ftp_upload(remote_upload, remote_download, source_upload, dest_download, ftp_host, ftp_user, ftp_password, port=21):
     try:
 
         # Connect to the FTP server
@@ -10,24 +10,24 @@ def ftp_upload(rem_up, rem_do, sc_up, dest_do, ftp_host, ftp_user, ftp_password,
         ftp.login(ftp_user, ftp_password)
 
         # List all files in source folder
-        filesSources = os.listdir(sc_up)
+        filesSources = os.listdir(source_upload)
 
         # Print the list of files
         for fileSc in filesSources:
-            up_path = rem_up + '/1234_' + fileSc 
-            with open(sc_up + '/' + fileSc, 'rb') as uploadFile:
+            up_path = remote_upload + '/1234_' + fileSc 
+            with open(source_upload + '/' + fileSc, 'rb') as uploadFile:
                 ftp.storbinary(f"STOR {up_path}", uploadFile)
 
         # Change to the download remote directory
-        ftp.cwd(rem_do)
+        ftp.cwd(remote_download)
         
         # List files in the remote directory
         files = ftp.nlst()
 
         # Download each file
         for filename in files:
-            local_file_path = f"{dest_do}/{filename}"
-            remote_file_path = f"{rem_do}/{filename}"
+            local_file_path = f"{dest_download}/{filename}"
+            remote_file_path = f"{remote_download}/{filename}"
             with open(local_file_path, 'wb') as local_file:
                 ftp.retrbinary(f"RETR {remote_file_path}", local_file.write)
 
