7302ff44f5addcf5c9cdfdda60346b2e421fb9c0
Iqbal Rizky <ntaloventi@gmail.com>
2023-10-18 04:46:02 +0700

    add log

--- ftp_docker.py
+++ ftp_docker.py
@@ -1,22 +1,37 @@
 import os
+from datetime import datetime
+import logging
 from ftplib import FTP, error_perm
 
-def ftp_upload(remote_upload, remote_download, source_upload, dest_download, ftp_host, ftp_user, ftp_password, port=21):
-    try:
+# Configure logging
+logging.basicConfig(
+    filename='audit.log',
+    level=logging.INFO,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
+def ftp_upload(remote_upload, remote_download, source_upload, dest_download, ftp_host, ftp_user, ftp_password, port=21):
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    try:
         # Connect to the FTP server
         ftp = FTP()
         ftp.connect(ftp_host, port)
         ftp.login(ftp_user, ftp_password)
+
+        logging.info("ftp connected")
 
         # List all files in source folder
         filesSources = os.listdir(source_upload)
 
         # Print the list of files
         for fileSc in filesSources:
-            up_path = remote_upload + '/1234_' + fileSc 
+            # up_path = remote_upload + '/' + timestamp + '_' + fileSc
+            up_path = remote_upload + '/' + fileSc 
             with open(source_upload + '/' + fileSc, 'rb') as uploadFile:
                 ftp.storbinary(f"STOR {up_path}", uploadFile)
+
+            logging.info('upload file: ' + up_path)
 
         # Change to the download remote directory
         ftp.cwd(remote_download)
@@ -30,6 +45,8 @@ def ftp_upload(remote_upload, remote_download, source_upload, dest_download, ftp
             remote_file_path = f"{remote_download}/{filename}"
             with open(local_file_path, 'wb') as local_file:
                 ftp.retrbinary(f"RETR {remote_file_path}", local_file.write)
+
+            logging.info('download file: ' + filename)
 
     except Exception as e:
         print(f"Error: {e}")
